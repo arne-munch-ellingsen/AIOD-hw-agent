b94cb8993c62a3dd119367d15683f66d28608f1a
Arne Munch-Ellingsen <70197048+arne-munch-ellingsen@users.noreply.github.com>
2024-05-16 22:27:28 +0200

    Update create_nfd_label.py
    Added some labels using names from the HW agent meta data model and prefix: ai4europe.aiod

--- nfd/create_nfd_label.py
+++ nfd/create_nfd_label.py
@@ -21,7 +21,17 @@ def create_nfd_tag(node_name, tag_key, tag_value):
     print(f"NFD tag {tag_key}={tag_value} added to node {node_name}")
 
 node_name = "odin"
-tag_key = "feature.node.kubernetes.io/my-tag"  # Replace with your desired tag key
-tag_value = "hello-mytag"  # Replace with your desired tag value
 
+# Create some tags with names taken from the HW agent metadata model
+tag_key = "ai4europe.aiod/id"
+tag_value = "telenor-edge-norway-tromso-1"
 create_nfd_tag(node_name, tag_key, tag_value)
+
+tag_key = "ai4europe.aiod/name"
+tag_value = "tromso-1"
+create_nfd_tag(node_name, tag_key, tag_value)
+
+tag_key = "ai4europe.aiod/geographical-location"
+tag_value = "69.65691644431789-18.926948341998617"
+create_nfd_tag(node_name, tag_key, tag_value)
+
